dd96f9cdc721e581b8cd1fbff1300d779038791f
Ethan Jaffe <jaffe413@gmail.com>
2020-04-02 18:21:35 +0300

    added ESLint - Code Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const nconf = require('nconf');
 
 nconf.argv()
-    .env({lowerCase: true, whitelist: ['projects_folder']});
+    .env({ lowerCase: true, whitelist: ['projects_folder'] });
 
-const {getAllProjects, getProjectFiles, createProject} = require('./src/project');
+const { getAllProjects, getProjectFiles, createProject } = require('./src/project');
 
-module.exports = {getAllProjects, getProjectFiles, createProject};
+module.exports = { getAllProjects, getProjectFiles, createProject };
--- src/project.js
+++ src/project.js
@@ -1,20 +1,21 @@
-const nconf = require('nconf');
 const fs = require('fs');
+const nconf = require('nconf');
+
 const fsPromises = fs.promises;
 
 if (!nconf.get('projects_folder')) {
     throw new Error('PROJECTS_FOLDER is undefined');
 }
 /**
- * Creates a new Folder in File System using **process.env.PROJECTS_FOLDER** and given **projectName**
+ * Creates a new Folder in File System using **process.env.PROJECTS_FOLDER** and given
+ * **projectName**
  * @param {string} projectName
- * @returns {boolean} - return **true** if a new project was created, **false** if it already exists.
+ * @return {boolean} - return **true** if a new project was created, **false** if it already exists.
  */
 const createProject = async (projectName) => {
     const projectsFolder = nconf.get('projects_folder');
     const path = `${projectsFolder}/${projectName}`;
 
-    const pathExists = fs.exist
     try {
         await fsPromises.mkdir(path);
         return true;
@@ -26,35 +27,30 @@ const createProject = async (projectName) => {
 
 /**
  * Retrieves a list of all existing projects in the File System
- * @returns {string[]}
+ * @return {string[]}
  */
 const getAllProjects = async () => {
     const path = nconf.get('projects_folder');
 
     const pathContents = await fsPromises.readdir(path);
-    const projects = pathContents.filter(async (file) => {
-        return await fsPromises.stat(`${path}/${file}`).isDirectory();
-    });
-    return projects;
+    return pathContents.filter(async (file) => fsPromises.stat(`${path}/${file}`).isDirectory());
 };
 
 /**
  * Retrieves the *filenames* inside the directory of **projectName**
  * @param projectName
- * @returns string[]
+ * @return string[]
  */
 const getProjectFiles = async (projectName) => {
     const projectsFolder = nconf.get('projects_folder');
     const path = `${projectsFolder}/${projectName}`;
 
-    const fsStat = await fsPromises.stat(path)
-
+    const fsStat = await fsPromises.stat(path);
     if (fsStat.isDirectory()) {
-        const filenames = await fsPromises.readdir(path);
-        return filenames;
+        return fsPromises.readdir(path);
     }
 
     throw new Error('Project does not exist');
 };
 
-module.exports = {createProject, getProjectFiles, getAllProjects};
+module.exports = { createProject, getProjectFiles, getAllProjects };
